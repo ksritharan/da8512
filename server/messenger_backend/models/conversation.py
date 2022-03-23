0735a6f56a975846a76f52d8e61473463badedca
ksritharan <kavin.sritharan@mail.utoronto.ca>
2022-03-23 18:41:09 -0400

    Changes as per PR review #7 - Database - ref #6
    Added many to many field for users to conversation

--- server/messenger_backend/models/conversation.py
+++ server/messenger_backend/models/conversation.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.db.models import Q
 
 from . import utils
-from .user import User
+from .participant import Participant
 
 
 class Conversation(utils.CustomModel):
+    users = models.ManyToManyField(Participant)
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True)
