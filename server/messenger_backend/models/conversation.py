1135598b1ef2eee6cee4d6f803af9af87fe670a0
ksritharan <kavin.sritharan@mail.utoronto.ca>
2022-03-23 19:08:50 -0400

    Revert "Changes as per PR review #7 - Database - ref #6"
    This reverts commit 0735a6f56a975846a76f52d8e61473463badedca.

--- server/messenger_backend/models/conversation.py
+++ server/messenger_backend/models/conversation.py
@@ -2,10 +2,9 @@ from django.db import models
 from django.db.models import Q
 
 from . import utils
-from .participant import Participant
+from .user import User
 
 
 class Conversation(utils.CustomModel):
-    users = models.ManyToManyField(Participant)
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True)
--- server/messenger_backend/models/participant.py
+++ server/messenger_backend/models/participant.py
@@ -1,12 +1,20 @@
 from django.db import models
 
 from . import utils
+from .conversation import Conversation
 from .user import User
 
 
 class Participant(utils.CustomModel):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, db_column="userId", related_name="+"
+    )
+    conversation = models.ForeignKey(
+        Conversation,
+        on_delete=models.CASCADE,
+        db_column="conversationId",
+        related_name="participants",
+        related_query_name="participant"
     )
     lastReadMessageId = models.IntegerField(blank=True, null=True)
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
